2d82312db8b265ccc0987673d391153331216c3f
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-12-22 16:41:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
                                  
 @app.route('/')
 def main():
