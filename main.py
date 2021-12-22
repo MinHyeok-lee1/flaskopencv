8ea77cbe38b25d67dc3025e5fa69848dd60fe029
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-12-22 17:00:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,15 @@
-#Import necessary libraries
 from flask import Flask, render_template, Response
 import cv2
-import mediapipe as mp
-import os 
 
-#Initialize the Flask app
 app = Flask(__name__)
-           
-@app.route('/')
-def main():
-    return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')  # use 0 for web camera
+#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+# for local webcam use cv2.VideoCapture(0)
 
-@app.route('/video_feed2')
-def video_feed2():
-    return Response(gen_opencv(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
-camera = cv2.VideoCapture(0)
-
-def gen_opencv():  
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while camera.isOpened():
-            ret, frame = camera.read()  # read the camera frame
-            # Recolor Feed
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Make Detections
-            results = holistic.process(image)
-            # print(results.face_landmarks)
-            
-            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-            
-            # Recolor image back to BGR for rendering
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            # 1. Draw face landmarks
-            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                                    mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                                    mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                                    )
-            
-            # 2. Right hand
-            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                    )
-
-            # 3. Left Hand
-            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                    )
-
-            # 4. Pose Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                    )
-            ret, buffer = cv2.imencode('.jpg', image)
-            image = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')  # concat frame one by one and show result
- 
-def gen_frames():  
+def gen_frames():  # generate frame by frame from camera
     while True:
+        # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
@@ -77,6 +18,19 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-            
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 8080), debug=False)
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
