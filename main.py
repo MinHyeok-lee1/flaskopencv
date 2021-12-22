f08af7b37543b3a29c7ce0ffb68ebff18cd9b464
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-12-22 16:35:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,10 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response
-import cv2
-import mediapipe as mp
 import os 
 
 #Initialize the Flask app
 app = Flask(__name__)
-           
-@app.route('/')
-def main():
-    return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_feed2')
-def video_feed2():
-    return Response(gen_opencv(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
-camera = cv2.VideoCapture(0)
-
-def gen_opencv():  
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while camera.isOpened():
-            ret, frame = camera.read()  # read the camera frame
-            # Recolor Feed
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Make Detections
-            results = holistic.process(image)
-            # print(results.face_landmarks)
-            
-            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-            
-            # Recolor image back to BGR for rendering
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            # 1. Draw face landmarks
-            mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                                    mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-                                    mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                                    )
-            
-            # 2. Right hand
-            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                    )
-
-            # 3. Left Hand
-            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                    )
-
-            # 4. Pose Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                    )
-            ret, buffer = cv2.imencode('.jpg', image)
-            image = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')  # concat frame one by one and show result
- 
 def gen_frames():  
     while True:
         success, frame = camera.read()  # read the camera frame
@@ -76,6 +15,15 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+                                 
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("PORT", 8080), debug=False)
